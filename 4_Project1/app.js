b10dd059dd6793c2bcf69e25a2e9340829a97c57
Qaidjohar <qaidjawadwala@gmail.com>
2019-10-25 19:58:55 +0530

    Attack Done Game1

--- 4_Project1/app.js
+++ 4_Project1/app.js
@@ -4,5 +4,35 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false
+  },
+  methods: {
+    startGame: function() {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+    attack: function() {
+      var max = 10;
+      var min = 3;
+      var damage = Math.floor(Math.random() * max + 1, min);
+      this.monsterHealth -= damage;
+      if (this.monsterHealth <= 0) {
+        alert("You won!!");
+        this.gameIsRunning = false;
+        return;
+      }
+
+      var max = 12;
+      var min = 5;
+      var damage = Math.floor(Math.random() * max + 1, min);
+      this.playerHealth -= damage;
+      if (this.playerHealth <= 0) {
+        alert("You Lost!!");
+        this.gameIsRunning = false;
+      }
+    },
+    specialAttack: function() {},
+    heal: function() {},
+    giveUp: function() {}
   }
 });
